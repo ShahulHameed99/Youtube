c0cc798e83ca2a2c4226c8662ac59ea257a953c8
Shahul Hameed <ShahulHameed.sm@dsrc.in>
2024-01-01 17:03:29 +0530

    BaseProject Added

---
+++ Youtube/Youtube/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Youtube
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
